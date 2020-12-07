2b5c1a6c214a390e559aecaf51d8c5f80b3f3a95
Mr-xuyonghui <xioahui1078610347@126.com>
2020-12-07 21:56:48 +0800

    提交ddt数据驱动代码

--- unittest/test_ddt_demo_1.py
+++ unittest/test_ddt_demo_1.py
@@ -1,9 +1,38 @@
 import unittest
-from ddt import ddt,data,
+from ddt import ddt,data,unpack,file_data
 @ddt
 class Test_ddt(unittest.TestCase):
-    @data(10)
+    #通过ddt可以传入python任意类型的数据
+    #传入字符串，数据
+    #@data(10)
+    @data("name",10)
     def test01_onedata(self,args):
         print(args)
+
+    @data(["name","age","sex"])
+    @unpack
+    def test02_muldata(self,name,age,sex):
+        print("my namme is{},{} years old and a {}".format(name,age,sex))
+
+    @data(["name","age","sex"])
+    @unpack
+    def test03_muldata(self,*args):
+        print("通过nupack解包列表：",args)
+    #*作用同解包list一样
+    @data(*["name", "age", "sex"])
+    def test04_muldata(self, args):
+        print(args)
+
+    @data(["name", "age", "sex"])
+    #实参之后一个的话，形成为任意多个位置形参，数据会有问题
+    #实际传入为：(['name', 'age', 'sex'],)
+    def test05_muldata(self, *args):
+        print(args)
+    #通过file_data 可以导入json或者yaml格式文件
+    @file_data("data.json")
+    def test06_filedata(self,args):
+        print(args)
+
+
 if __name__ == '__main__':
     unittest.main()
--- unittest/test_partcase_demo_1.py
+++ unittest/test_partcase_demo_1.py
@@ -14,7 +14,7 @@ class Test_part(unittest.TestCase):
         print("这是第四个测试")
 if __name__ == '__main__':
     #指定默认运行某个测试用例，不指定默认为全部
-    #unittest.main(defaultTest='Test_part("test_baidu")')
+    #unittest.main(defaultTest=Test_part.test_baidu)
 
     #通过addTest单个添加测试用例
     # #TestSuite() 不要忘记（）
@@ -25,16 +25,16 @@ if __name__ == '__main__':
     # unittest.main(defaultTest='suite')
 
 
-    # suite = unittest.TestSuite()
-    # testcase = [Test_part("test_baidu"),Test_part("test_bilibili")]
-    # suite.addTests(testcase)
-    # #unittest.main(defaultTest='suite')
-    # TextTextRunner():unittest框架的TextTextRunner()类，通过该类下面的run()方法来运行suite所组装的测试用例，入参为suite测试套件。
-    # unittest.TextTestRunner().run(suite)
-
     suite = unittest.TestSuite()
-    #自动查看当前目录下的所有测试用例
-    testcases=unittest.defaultTestLoader.discover(start_dir = os.getcwd(),pattern='*.py')
-    print(testcases)
-    suite.addTests(testcases)
-    unittest.main(defaultTest='suite')
+    testcase = [Test_part("test_baidu"),Test_part("test_bilibili")]
+    suite.addTests(testcase)
+    #unittest.main(defaultTest='suite')
+    #TextTextRunner():unittest框架的TextTextRunner()类，通过该类下面的run()方法来运行suite所组装的测试用例，入参为suite测试套件。
+    unittest.TextTestRunner().run(suite)
+
+    # suite = unittest.TestSuite()
+    # #自动查看当前目录下的所有测试用例
+    # testcases=unittest.defaultTestLoader.discover(start_dir = os.getcwd(),pattern='*.py')
+    # print(testcases)
+    # suite.addTests(testcases)
+    # unittest.main(defaultTest='suite')
