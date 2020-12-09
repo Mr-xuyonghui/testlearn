01032414d9d5f1dfe60ae7849f555a2ac8596d65
Mr-xuyonghui <xioahui1078610347@126.com>
2020-12-09 22:02:11 +0800

    添加生成html测试报告代码

--- unittestlearn/test_partcase_demo_1.py
+++ unittestlearn/test_partcase_demo_1.py
@@ -1,6 +1,8 @@
+from _datetime import datetime
 import unittest
 import os
 from unittestlearn import test_skip_demo_1
+from unittestlearn.HTMLTestRunner import HTMLTestRunner
 class Test_part(unittest.TestCase):
     def test_baidu(self):
         print("这是第一个测试")
@@ -34,18 +36,29 @@ if __name__ == '__main__':
     #unittest.TextTestRunner().run(suite)
     # print("当前文件文件名",os.path.abspath(__file__))
     # #获取当前文件所在文件夹名称
-    # dir_name=os.path.dirname(os.path.abspath(__file__))
-    # report_path=os.path.join(dir_name,"report")
+    dir_name=os.path.dirname(os.path.abspath(__file__))
+    report_path=os.path.join(dir_name,"report")
     # #判断文件夹是否存在，不存在则创建
-    # if not os.path.exists(report_path):
-    #     os.mkdir(report_path)
+    if not os.path.exists(report_path):
+          os.mkdir(report_path)
+    #生成普通文档测试报告
     # file_path=os.path.join(report_path,"result.txt")
+    # #生成html格式测试报告
+    str_time = datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
+    file_name ="resule"+str_time+".html"
+    file_path = os.path.join(report_path, file_name)
     # with open(file_path,"w",encoding="utf-8")as f:
-          #初始化运行器
+    with open(file_path, "wb") as f:
+    #初始化运行器
+        runner = HTMLTestRunner(f,verbosity=2,
+                                title="这是个html格式的测试报告",
+                                description="测试报告"
+                                )
     #     runner=unittest.TextTestRunner(f,verbosity=2)
-    #     runner.run(suite)
-
+        runner.run(suite)
     suite = unittest.TestSuite()
+
+
     #自动查看当前目录下的所有测试用例
     #testcases=unittest.defaultTestLoader.discover(start_dir = os.getcwd(),pattern='*.py')
     #defaultTestLoader = TestLoader()
@@ -58,9 +71,9 @@ if __name__ == '__main__':
     #通过名称加载测试用例，入参为列表，元素为字符串
     #testcases = loader.loadTestsFromNames(['test_skip_demo_1.Test_skip.test_login','test_skip_demo_1.Test_skip.test_false','test_skip_demo_1.Test_skip.test_skip_1'])
     #通过类名加载测试用例 模块名.类名
-    testcases = loader.loadTestsFromTestCase(test_skip_demo_1.Test_skip)
+    #testcases = loader.loadTestsFromTestCase(test_skip_demo_1.Test_skip)
     #查找指定目录下的所有测试用例
     #testcases = loader.discover(start_dir=os.getcwd(),pattern='*.py')
-    print(testcases)
-    suite.addTests(testcases)
-    unittest.main(defaultTest='suite')
+    #print(testcases)
+    #suite.addTests(testcases)
+    #unittest.main(defaultTest='suite')
