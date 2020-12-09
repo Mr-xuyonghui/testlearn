ea43bd58a6bc4f3f30a6b38c9f2e4f2e1b83ab7e
Mr-xuyonghui <xioahui1078610347@126.com>
2020-12-09 22:46:12 +0800

    移除生成报告代码

--- unittestlearn/test_partcase_demo_1.py
+++ unittestlearn/test_partcase_demo_1.py
@@ -27,43 +27,21 @@ if __name__ == '__main__':
     # #defaultTest= '' 不要忘记单引号
     # unittest.main(defaultTest='suite')
 
-
+    #通过addTestcases添加测试用例
     suite = unittest.TestSuite()
     testcase = [Test_part("test_baidu"),Test_part("test_bilibili")]
     suite.addTests(testcase)
-    #unittest.main(defaultTest='suite')
+    unittest.main(defaultTest='suite')
     #TextTextRunner():unittest框架的TextTextRunner()类，通过该类下面的run()方法来运行suite所组装的测试用例，入参为suite测试套件。
     #unittest.TextTestRunner().run(suite)
-    # print("当前文件文件名",os.path.abspath(__file__))
-    # #获取当前文件所在文件夹名称
-    dir_name=os.path.dirname(os.path.abspath(__file__))
-    report_path=os.path.join(dir_name,"report")
-    # #判断文件夹是否存在，不存在则创建
-    if not os.path.exists(report_path):
-          os.mkdir(report_path)
-    #生成普通文档测试报告
-    # file_path=os.path.join(report_path,"result.txt")
-    # #生成html格式测试报告
-    str_time = datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
-    file_name ="resule"+str_time+".html"
-    file_path = os.path.join(report_path, file_name)
-    # with open(file_path,"w",encoding="utf-8")as f:
-    with open(file_path, "wb") as f:
-    #初始化运行器
-        runner = HTMLTestRunner(f,verbosity=2,
-                                title="这是个html格式的测试报告",
-                                description="测试报告"
-                                )
-    #     runner=unittest.TextTestRunner(f,verbosity=2)
-        runner.run(suite)
-    suite = unittest.TestSuite()
+
 
 
     #自动查看当前目录下的所有测试用例
     #testcases=unittest.defaultTestLoader.discover(start_dir = os.getcwd(),pattern='*.py')
     #defaultTestLoader = TestLoader()
     #初始化加载器
-    loader = unittest.TestLoader()
+    #loader = unittest.TestLoader()
     #指定查找某个模块里面的测试用例
     #testcases = loader.loadTestsFromModule(test_skip_demo_1)
     #通过名称加载测试用例，入参为字符串
