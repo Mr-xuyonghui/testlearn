79d4f986ddfa64cc2e08d684e4f959a6432c5f61
Mr-xuyonghui <xioahui1078610347@126.com>
2020-12-16 00:00:18 +0800

    修改request封装类代码

--- unittestlearn/request_class.py
+++ unittestlearn/request_class.py
@@ -1,17 +1,26 @@
 import  requests
 class Requestunitl():
-    def __init__(self,url):
-        self.url = url
-        self.headers={"Content-Type":"application/json;charset=utf-8"}
-    def _get(self,param,**kwargs):
-        res = requests.get(self.url,param=param)
+    def __init__(self):
+        #self.url = url
+        #self.headers={"Content-Type":"application/json;charset=utf-8"}
+        pass
+    def _get(self,url,param,**kwargs):
+        res = requests.get(url,params=param,**kwargs)
         return res
+        #   {
+        #     "responseheader" : res.headers,
+        #     "responsebody" : res.json()
+        # }
 
-    def _post(self,data,json=None,**kwargs):
-        res = requests.post(self.url,data=data,json=json)
+    def _post(self,url,data,json=None,**kwargs):
+        res = requests.post(url,data=data,json=json,**kwargs)
         return res
+        #     {
+        #     "responseheader" : res.headers,
+        #     "responsebody" : res.json()
+        # }
 
 if __name__ == '__main__':
-    request =Requestunitl('https://api.s.suv666.com/iyourcar_news_suv/suv/news/related_jxwd/list')
-    result =request._post(data={"artilce_online_id":"871611"})
-    print(result.json())
+    request =Requestunitl()
+    result =request._post('https://api.s.suv666.com/iyourcar_news_suv/suv/news/related_jxwd/list',data={"artilce_online_id":"871611"})
+    print(result)
