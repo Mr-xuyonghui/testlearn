f7bc8af664acd8a43d67438b27c038442a01793a
Mr-xuyonghui <xioahui1078610347@126.com>
2020-12-16 21:25:00 +0800

    添加获取头部信息代码

--- unittestlearn/request_class.py
+++ unittestlearn/request_class.py
@@ -24,3 +24,17 @@ if __name__ == '__main__':
     request =Requestunitl()
     result =request._post('https://api.s.suv666.com/iyourcar_news_suv/suv/news/related_jxwd/list',data={"artilce_online_id":"871611"})
     print(result)
+    #获取响应状态码
+    print("响应状态码:",result.status_code)
+    #获取请求头部
+    print("request-header：",result.request.headers)
+    #获取请求头的cookies
+    print("request-cookies：", result.request._cookies)
+    #字符串化处理,二进制，json格式response
+    print(result.text)
+    print(result.content)
+    print(result.json())
+    #获取响应体头部
+    print(result.headers)
+    #获取响应体cookies
+    print(result.cookies)
